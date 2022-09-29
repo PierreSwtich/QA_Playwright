f31e99afe3230ca6fe80b94ee91fa185c8592a52
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-29 19:27:42 +0200

    Delete example tests, managing config file

--- playwright.config.js
+++ playwright.config.js
@@ -15,13 +15,13 @@ const { devices } = require('@playwright/test');
 const config = {
   testDir: './E2E_example',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 10 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 10 * 1000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
