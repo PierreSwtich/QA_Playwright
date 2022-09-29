880c028fd80d632d44e94fee6305734804cf794d
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-29 19:38:29 +0200

    Completing 1st test and creating new test

--- E2E_example/My_First_Test.spec.js
+++ E2E_example/My_First_Test.spec.js
@@ -5,24 +5,21 @@ test.beforeAll(async () => {
     console.log('BEFORE ALL')
 })
 
-    test('Test - 1', async ()=> {
+test('Test - 1', async ()=> {
         console.log('-------------')
         console.log('TEST - 1')
         console.log('-------------')
+})
 
-    })
-
-    test('Test - 1', async ()=> {
-        console.log('-------------')
-        console.log('TEST - 1')
-        console.log('-------------')
-
-    })
-
-    test('Test - 2', async ()=> {
+test('Test - 2', async ()=> {
         console.log('-------------')
         console.log('TEST - 2')
         console.log('-------------')
+})
 
-    })
+test('Test - 3', async ()=> {
+        console.log('-------------')
+        console.log('TEST - 3')
+        console.log('-------------')
+})
 
