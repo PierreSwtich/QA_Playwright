2aa96d993138a657a9c00cbb249c46f7ecbfc86e
Pierre Swtich <a.stolarski.as@gmail.com>
2022-09-29 19:33:06 +0200

    Commenting other browsers in in config file Adding Test 1

---
+++ E2E_example/My_First_Test.spec.js
@@ -0,0 +1,28 @@
+const { test, expect } = require('@playwright/test');
+
+
+test.beforeAll(async () => {
+    console.log('BEFORE ALL')
+})
+
+    test('Test - 1', async ()=> {
+        console.log('-------------')
+        console.log('TEST - 1')
+        console.log('-------------')
+
+    })
+
+    test('Test - 1', async ()=> {
+        console.log('-------------')
+        console.log('TEST - 1')
+        console.log('-------------')
+
+    })
+
+    test('Test - 2', async ()=> {
+        console.log('-------------')
+        console.log('TEST - 2')
+        console.log('-------------')
+
+    })
+
